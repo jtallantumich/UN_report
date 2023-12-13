a0badfc27abaea67665753e5df7476a7b48b88c7
Jason Tallant <jtallantumich@users.noreply.github.com>
2023-12-13 15:25:17 -0500

    initial commit

---
+++ Code/gdp_population.R
@@ -0,0 +1,68 @@
+library(tidyverse)
+
+gapminder_1997 <- read_csv("gapminder_1997.csv")
+read
+View(gapminder_1997)
+
+name <- "Ben"
+name
+
+age <- 26
+age
+
+name_character <- "Harry Potter"
+  
+#test <- read_csv("gapminder_1997.csv")
+
+Sys.Date() #outputs the current date, used for knowing when I last ran 
+
+getwd() #output current directory
+
+sum(5, 6) # adds numbers
+
+round(3.1415, 2)
+
+read_csv(file = 'gapminder_1997.csv')
+
+ggplot(data=gapminder_1997) + 
+  aes(x = gdpPercap) +
+  labs(x = "GDP Per Capita") +
+  aes(y = lifeExp) +
+  labs(y = "Life Expectancy") +
+  geom_point() +
+  labs(title = "Do people in wealthy countries live longer?") +
+  aes(color = continent) +
+  scale_color_brewer(palette = "Set1") +
+  aes(size = pop/1000000) +
+  labs(size = "Population (in millions)") +
+  aes(shape = continent)
+
+ggplot(data = gapminder_1997) +
+  aes(gdpPercap, lifeExp, color = continent, size = pop/1000000, shape = continent) +
+  geom_point() +
+  scale_color_brewer(palette = "Set1") +
+  labs(x ="GDP Per Capita", y = "Life Expentancy", 
+       title = "Do people in wealthy countries live longer?",
+       size = "Population (in millions)")
+
+gapminder_data <- read_csv("gapminder_data.csv")
+
+dim(gapminder_data)
+head(gapminder_data)
+
+#X = year
+#Y = lifeExp
+#color = continent
+#geom_point
+
+ggplot(data = gapminder_data) +
+  aes(x = year, y = lifeExp, color = continent) +
+  geom_point()
+
+
+
+
+
+
+
+
